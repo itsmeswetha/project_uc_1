463839d261368cb7f2e002d85a5ada41f5abaef3
itsmeswetha <swethamanasap@gmail.com>
2023-10-27 16:00:54 +0530

    added firewall

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,3 +12,13 @@ resource "google_compute_subnetwork" "network-subnet" {
   network = google_compute_network.network
   ip_cidr_range = var.subnet_cidr
 }
+
+# Creating a firewall rule
+
+resource "google_compute_firewall" "network-firewall" {
+  name = var.firewall_name
+  network = google_compute_network.network
+  target_tags = var.target_tag
+  source_ranges = var.source_ranges
+  source_tags = var.source_tags
+}
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -11,3 +11,15 @@ variable "subnet_cidr" {
 variable "subnet_region" {
   
 }
+variable "firewall_name" {
+  
+}
+variable "source_ranges" {
+  
+}
+variable "target_tag" {
+  
+}
+variable "source_tags" {
+  
+}
