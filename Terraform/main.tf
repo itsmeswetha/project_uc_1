64445be04d4aebeff5f8ade01bdb4a62a0858178
itsmeswetha <swethamanasap@gmail.com>
2023-10-31 15:33:28 +0530

    added provider

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.4.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+}
+
 # Creating a VPC
 
 resource "google_compute_network" "network" {
@@ -18,7 +31,13 @@ resource "google_compute_subnetwork" "network-subnet" {
 resource "google_compute_firewall" "network-firewall" {
   name = var.firewall_name
   network = google_compute_network.network
-  target_tags = var.target_tag
+  target_tags = var.target_tag  
   source_ranges = var.source_ranges
   source_tags = var.source_tags
+
+  allow {
+    protocol = var.proto
+    ports    = var.port
+  }
 }
+
