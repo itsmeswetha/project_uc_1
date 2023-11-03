e681783fd428ca5dbd398f114450cf6bf75206d8
swethamanasa.poolla <swethamanasa.poolla@gitlab.cloudexpertsguru.com>
2023-11-03 11:40:59 +0530

    added directory structure to gitlab

--- VPC/main.tf
+++ VPC/main.tf
@@ -4,8 +4,10 @@
 
 resource "google_compute_network" "network" {
   name = var.vpc_name
-  routing_mode = Global
+  routing_mode = GLOBAL
   auto_create_subnetworks = false
+  mtu = 1460
+  project = var.projectid
 }
 
 
--- VPC/variables.tf
+++ VPC/variables.tf
@@ -2,30 +2,6 @@ variable "vpc_name" {
   
 }
 
-variable "subnet_name" {
+variable "projectid" {
   
 }
-variable "subnet_cidr" {
-  
-}
-variable "subnet_region" {
-  
-}
-variable "firewall_name" {
-  
-}
-variable "source_ranges" {
-  
-}
-variable "target_tag" {
-  
-}
-variable "source_tags" {
-  
-}
-variable "project_id" {
-  
-}
-variable "port" {
-    type = list(string)
-  }
